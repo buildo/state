1bcb82149cd45d70dc96d1329ea59fb201726326
Vincenzo Guerrisi <vincenzo@buildo.io>
2017-05-26 16:41:19 +0200

    add implicit state variable __k to stateType

--- src/index.js
+++ src/index.js
@@ -11,8 +11,13 @@ export default stateType => {
     throw new Error('`stateType` must be a strict tcomb interface');
   }
 
+  const StateType = t.interface({
+    ___k: t.maybe(t.Number),
+    ...stateType.meta.props
+  }, { strict: true, name: stateType.meta.name });
+
   return {
-    run: run(stateType),
-    connect: connect(stateType)
+    run: run(StateType),
+    connect: connect(StateType)
   };
 };

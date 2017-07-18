56e75768df008f3b1edc7effc8f35197060d8b54
Giovanni Gonzaga <giovanni@buildo.io>
2017-07-18 18:46:21 +0200

    update default flushTimeout and add warning

--- src/run.js
+++ src/run.js
@@ -37,6 +37,9 @@ export function createProvideWrapper({
     }
     flushTimeout = setTimeout(() => {
       if (queue.length > 0 && queue[0].time <= Date.now() - flushTimeoutMSec) {
+        if (process.env.NODE_ENV !== 'production') {
+          console.warn('state:flushTimeout', 'flushTimeout expired! We probably were expecting an update from the router that did\'t call us instead :\'('); // eslint-disable-line no-console
+        }
         setPendingState(null);
         maybeRunQueuedTransition();
         updateFlushTimeout();
@@ -206,7 +209,7 @@ export default stateType => ({
   //
   // Number
   //
-  flushTimeoutMSec = 200,
+  flushTimeoutMSec = 5000,
 
   // whether to serialize (sync to browser) a state key or not
   //

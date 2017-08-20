d245fa4ff91a125e3bac29343060cc052b5bc68e
Giovanni Gonzaga <giovanni@buildo.io>
2017-08-20 11:51:59 +0200

    add more tests and fix URI encoding for browser functions

--- src/browser.ts
+++ src/browser.ts
@@ -1,27 +1,33 @@
-import { History, Action } from 'history';
+import { History, Location, Action } from 'history';
 import createBrowserHistory from 'history/createBrowserHistory';
 import trim = require('lodash/trim');
 import omit = require('lodash/omit');
 import * as qs from 'qs';
 
+// TODO: below, ignoring path params for the moment
+// there's only `view` which is the whole `pathname` from history perspective
+
 export type BrowserState = { [k: string]: string } & { view: string };
 
+// export is for tests only
+// TODO(gio): `location` here is optional just because (I think)
+// it can be undefined in some tests using `memoryHistory`
+export function parseBrowserState(location?: Location): BrowserState {
+  const view = location ? trim(location.pathname, ' /') : '';
+  const s = location ? qs.parse(trim(location.search, ' ?')) : {};
+  return { ...s, view };
+}
+
+// export is for tests only
+export function serializeBrowserState(s: BrowserState): Partial<Location> {
+  const pathname = `/${encodeURIComponent(s.view)}`;
+  const search = `?${qs.stringify(omit(s, ['view']))}`; // will encode URI by default
+  return { pathname, search };
+}
+
 export default function(history: History = createBrowserHistory()) {
-  // TODO: below, ignoring path params for the moment
-  // there's only `view` which is the whole `pathname` from history perspective
-
   function syncToBrowser(s: BrowserState, push: boolean = true): void {
-    const pathname = `/${s.view}`;
-    const search = `?${qs.stringify(omit(s, ['view']))}`;
-    (push ? history.push : history.replace)({ pathname, search });
-  }
-
-  // TODO(gio): `location` here is optional just because (I think)
-  // it can be undefined in some tests using `memoryHistory`
-  function parseBrowserState(location?: typeof history.location): BrowserState {
-    const view = location ? trim(location.pathname, ' /') : '';
-    const s = location ? qs.parse(location.search, { ignoreQueryPrefix: true }) : {};
-    return { ...s, view };
+    (push ? history.push : history.replace)(serializeBrowserState(s));
   }
 
   function onBrowserChange(callback: (s: BrowserState, action: Action) => void): void {
--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,5 +1,17 @@
-import mkBrowser from '../src/browser';
+import mkBrowser, { parseBrowserState, serializeBrowserState } from '../src/browser';
 import createMemoryHistory from 'history/createMemoryHistory';
+import { Location } from 'history';
+
+function partialLocation(pl: Partial<Location>): Location {
+  return {
+    pathname: '/',
+    search: '?',
+    hash: '#',
+    key: 'foobar',
+    state: null,
+    ...pl
+  };
+}
 
 describe('browser', () => {
   describe('syncToBrowser', () => {
@@ -29,11 +41,72 @@ describe('browser', () => {
     });
   });
 
-  it('onBrowserChange should be called synchornously after a syncToBrowser', () => {
-    const onBrowserChangeSpy = jest.fn();
-    const { syncToBrowser, onBrowserChange } = mkBrowser(createMemoryHistory());
-    onBrowserChange(onBrowserChangeSpy);
-    syncToBrowser({ view: 'foo', foo: 'bar' }, false);
-    expect(onBrowserChangeSpy.mock.calls.length).toBe(1 + 1);
+  describe('onBrowserChange', () => {
+    it('should be called synchronously after a syncToBrowser push=true, with action=PUSH', () => {
+      const onBrowserChangeSpy = jest.fn();
+      const { syncToBrowser, onBrowserChange } = mkBrowser(createMemoryHistory());
+      onBrowserChange(onBrowserChangeSpy);
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(1);
+      syncToBrowser({ view: 'foo', foo: 'bar' }, true);
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(1 + 1);
+      expect(onBrowserChangeSpy.mock.calls[onBrowserChangeSpy.mock.calls.length - 1]).toEqual([
+        { view: 'foo', foo: 'bar' },
+        'PUSH'
+      ]);
+    });
+
+    it('should be called synchronously after a syncToBrowser push=false, with action=REPLACE', () => {
+      const onBrowserChangeSpy = jest.fn();
+      const { syncToBrowser, onBrowserChange } = mkBrowser(createMemoryHistory());
+      onBrowserChange(onBrowserChangeSpy);
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(1);
+      syncToBrowser({ view: 'foo', foo: 'bar' }, false);
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(1 + 1);
+      expect(onBrowserChangeSpy.mock.calls[onBrowserChangeSpy.mock.calls.length - 1]).toEqual([
+        { view: 'foo', foo: 'bar' },
+        'REPLACE'
+      ]);
+    });
+
+    it('should be called synchronously after a browser back, with action=POP', () => {
+      const onBrowserChangeSpy = jest.fn();
+      const history = createMemoryHistory();
+      const { syncToBrowser, onBrowserChange } = mkBrowser(history);
+      onBrowserChange(onBrowserChangeSpy);
+      syncToBrowser({ view: 'foo', foo: 'bar' }, false);
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(2);
+      history.goBack();
+      expect(onBrowserChangeSpy.mock.calls.length).toBe(2 + 1);
+      expect(onBrowserChangeSpy.mock.calls[onBrowserChangeSpy.mock.calls.length - 1]).toEqual([
+        { view: 'foo', foo: 'bar' },
+        'POP'
+      ]);
+    });
+  });
+
+  describe('parseBrowserState', () => {
+    it('should trim slashes from view param', () => {
+      expect(parseBrowserState(partialLocation({ pathname: '/view//' }))).toEqual({ view: 'view' });
+    });
+
+    it('should trim question marks from search params', () => {
+      expect(parseBrowserState(partialLocation({ search: '??foo=foo&bar=bar' }))).toEqual({
+        view: '',
+        foo: 'foo',
+        bar: 'bar'
+      });
+    });
+  });
+
+  describe('serializeBrowserState', () => {
+    it('should produce urlencoded pathnames with a single leading /', () => {
+      expect(serializeBrowserState({ view: 'foo' }).pathname).toEqual('/foo');
+      expect(serializeBrowserState({ view: '/foo/' }).pathname).toEqual('/%2Ffoo%2F');
+    });
+
+    it('should produce urlencoded searches with a single leading ?', () => {
+      expect(serializeBrowserState({ view: 'foo', foo: 'foo', bar: 'bar' }).search).toBe('?foo=foo&bar=bar');
+      expect(serializeBrowserState({ view: 'foo', '?foo': 'foo', bar: 'bar' }).search).toBe('?%3Ffoo=foo&bar=bar');
+    });
   });
 });
